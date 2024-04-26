d3096bfd8e2faf7e1c9391180ff33028f0aa7a70
publio.diaz <p.diazp@uniandes.edu.co>
2024-04-25 22:33:19 -0500

    feat: Definicion de modulo, componente y objeto cafe

---
+++ angular-varidades-cafe/src/app/cafe/cafe.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CafeComponent } from './cafe.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CafeComponent]
+})
+export class CafeModule { }
